7c35f41b66d09672557fca22801cb99f451c97aa
An <1162734640@qq.com>
2022-06-20 14:38:37 +0800

    fix: md文档格式

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ set -e
 # 生成静态文件
 npm run build
 
-# 进入生成的文件夹
+## 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # deploy to github
@@ -16,11 +16,11 @@ git config --global user.name "An"
 git init
 git add .
 git commit -m 'build: deploy'
-git remote add origin https://chuanyuan_an:acy123456@gitee.com/chuanyuan_an/vuepress_blog.git
-git remote add github https://github.com/anchuanyuan/anchuanyuan.github.io.git
-git push origin main -f
+#git remote add origin https://chuanyuan_an:acy123456@gitee.com/chuanyuan_an/vuepress_blog.git
+git remote add github git@github.com:anchuanyuan/anchuanyuan.github.io.git
+#git push origin main -f
 git push github master -f
 
 
 cd - # 退回开始所在目录
-rm -rf docs/.vuepress/dist
+#rm -rf docs/.vuepress/dist
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'yuan',
   description: 'yuan',
-  base: '/vuepress_blog/', // for Gitee
+  base: '/', // for Gitee
   // base: '/', // for GitHub
   head: [
     [
@@ -89,52 +89,96 @@ module.exports = {
     lineNumbers: true
   },
   plugins: [
+    // [
+    //   '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+    //   {
+    //     theme: ['z16'],
+    //     clean: false,
+    //     messages: {
+    //       welcome: '欢迎你的关注 ',
+    //       home: '心里的花，我想要带你回家。',
+    //       theme: '好吧，希望你能喜欢我的其他小伙伴。',
+    //       close: '再见哦'
+    //     }
+    //   }
+    // ],
+    // [
+    //   '@vuepress-reco/vuepress-plugin-bgm-player',
+    //   {
+    //     audios: [
+    //       {
+    //         name: '御鹿神谷、漆柚 - 功成名就',
+    //         artist: '收听列表',
+    //         url: 'http://chuanyuan_an.gitee.io/tuchuang/music/御鹿神谷、漆柚 - 功成名就.mp3',
+    //         cover:
+    //           'http://p1.music.126.net/oDiEku7wY0a3BWRXFA7Gqg==/109951164530191765.jpg?param=300x300'
+    //       },
+    //       {
+    //         name: '李荣浩 - 大城小爱 (Live)',
+    //         artist: '收听列表',
+    //         url: 'http://chuanyuan_an.gitee.io/tuchuang/music/李荣浩 - 大城小爱 (Live).mp3',
+    //         cover:
+    //           'https://y.gtimg.cn/music/photo_new/T002R300x300M000002OR8wD3Lo3E5_1.jpg?max_age=2592000?param=300x300'
+    //       },
+    //       {
+    //         name: '林俊杰 - 交换余生',
+    //         artist: ' 收听列表',
+    //         url: 'http://chuanyuan_an.gitee.io/tuchuang/music/林俊杰 - 交换余生.mp3',
+    //         cover:
+    //           'http://p1.music.126.net/HBzkUXwTlbUsH6OJvdWCVg==/109951164927567872.jpg?param=300x300'
+    //       },
+    //       {
+    //         name: '西楼别序-尹昔眠,小田音乐社',
+    //         artist: ' 收听列表',
+    //         url: 'http://chuanyuan_an.gitee.io/tuchuang/music/西楼别序-尹昔眠,小田音乐社.mp3',
+    //         cover:
+    //           'http://p1.music.126.net/oDiEku7wY0a3BWRXFA7Gqg==/109951164530191765.jpg?param=300x300'
+    //       }
+    //     ]
+    //   }
+    // ]
     [
-      '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+      'vuepress-plugin-nuggets-style-copy',
       {
-        theme: ['z16'],
-        clean: false,
-        messages: {
-          welcome: '欢迎你的关注 ',
-          home: '心里的花，我想要带你回家。',
-          theme: '好吧，希望你能喜欢我的其他小伙伴。',
-          close: '再见哦'
+        copyText: '复制代码',
+        tip: {
+          content: '复制成功!'
         }
       }
     ],
+    // 音乐播放器
     [
-      '@vuepress-reco/vuepress-plugin-bgm-player',
+      'meting',
       {
-        audios: [
-          {
-            name: '御鹿神谷、漆柚 - 功成名就',
-            artist: '收听列表',
-            url: 'http://chuanyuan_an.gitee.io/tuchuang/music/御鹿神谷、漆柚 - 功成名就.mp3',
-            cover:
-              'http://p1.music.126.net/oDiEku7wY0a3BWRXFA7Gqg==/109951164530191765.jpg?param=300x300'
-          },
-          {
-            name: '李荣浩 - 大城小爱 (Live)',
-            artist: '收听列表',
-            url: 'http://chuanyuan_an.gitee.io/tuchuang/music/李荣浩 - 大城小爱 (Live).mp3',
-            cover:
-              'https://y.gtimg.cn/music/photo_new/T002R300x300M000002OR8wD3Lo3E5_1.jpg?max_age=2592000?param=300x300'
-          },
-          {
-            name: '林俊杰 - 交换余生',
-            artist: ' 收听列表',
-            url: 'http://chuanyuan_an.gitee.io/tuchuang/music/林俊杰 - 交换余生.mp3',
-            cover:
-              'http://p1.music.126.net/HBzkUXwTlbUsH6OJvdWCVg==/109951164927567872.jpg?param=300x300'
-          },
-          {
-            name: '西楼别序-尹昔眠,小田音乐社',
-            artist: ' 收听列表',
-            url: 'http://chuanyuan_an.gitee.io/tuchuang/music/西楼别序-尹昔眠,小田音乐社.mp3',
-            cover:
-              'http://p1.music.126.net/oDiEku7wY0a3BWRXFA7Gqg==/109951164530191765.jpg?param=300x300'
-          }
-        ]
+        meting: {
+          // 网易
+          server: 'netease',
+          // 读取歌单列表
+          type: 'playlist',
+          mid: '322361152'
+        },
+        // 不配置该项的话不会出现全局播放器
+        aplayer: {
+          // 吸底模式
+          fixed: true,
+          mini: true,
+          // 自动播放
+          autoplay: true,
+          // 歌曲栏折叠
+          listFolded: true,
+          // 颜色
+          theme: '#f9bcdd',
+          // 播放顺序为随机
+          order: 'random',
+          // 初始音量
+          volume: 0.1,
+          // 关闭歌词显示
+          lrcType: 0
+        },
+        mobile: {
+          // 手机端去掉cover图
+          cover: false
+        }
       }
     ]
   ]
